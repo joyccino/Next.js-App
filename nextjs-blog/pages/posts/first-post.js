096e95b0786fb1570bf4c0816fb67e8baaffef28
Joyccino <67300266+joyccino@users.noreply.github.com>
2024-10-02 21:13:35 +0900

    update the FirstPost component to include the Script component

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import Head from 'next/head';
 import Link from 'next/link';
 
@@ -6,8 +8,13 @@ export default function FirstPost() {
       <>
         <Head>
             <title>First Post</title>
-            <script src="https://connect.facebook.net/en_US/sdk.js" />
         </Head>
+        <Script src="https://connect.facebook.net/en_US/sdk.js" 
+            strategy="lazyOnload"
+            onLoad={() =>
+                console.log(`script loaded correctly, window.FB has been populated`)
+            }
+        />
         <h1>First Post</h1>
         <h2>
           <Link href="/">← Back to home</Link>
