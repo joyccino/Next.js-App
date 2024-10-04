2bd74ca32a745914b4f287c6bd6c1e5854d294e5
Joyccino <67300266+joyccino@users.noreply.github.com>
2024-10-04 20:00:59 +0900

    add 'await' when calling getPostData accordingly

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -3,13 +3,15 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+  
+    return {
+      props: {
+        postData,
+      },
+    };
+  }
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
