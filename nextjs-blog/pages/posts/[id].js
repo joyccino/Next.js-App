16ff1c90e499747513e362d4f5970e26a63ae2c6
Joyccino <67300266+joyccino@users.noreply.github.com>
2024-10-04 20:11:10 +0900

    css 적용

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout';
 import Head from 'next/head';
 import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
@@ -26,14 +27,15 @@ export default function Post({ postData }) {
     return (
       <Layout>
         <Head>
-            <title>{postData.title}</title>
+          <title>{postData.title}</title>
         </Head>
-        <Date dateString={postData.date} />
-        {postData.id}
-        <br />
-        {postData.date}
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
       </Layout>
     );
   }
